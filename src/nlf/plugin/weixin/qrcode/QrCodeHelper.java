dd18fecc30dbe21189e2c265c583925a1871f657
6tail <6tail@newtail>
2014-12-23 17:13:17 +0800

    增加二维码相关功能

---
+++ src/nlf/plugin/weixin/qrcode/QrCodeHelper.java
@@ -0,0 +1,73 @@
+package nlf.plugin.weixin.qrcode;
+
+import nc.liat6.frame.db.entity.Bean;
+import nc.liat6.frame.json.JSON;
+import nc.liat6.frame.log.Logger;
+import nc.liat6.frame.util.Stringer;
+import nlf.plugin.weixin.exception.WeixinException;
+import nlf.plugin.weixin.qrcode.bean.QrCodeRequest;
+import nlf.plugin.weixin.qrcode.bean.QrCodeResponse;
+import nlf.plugin.weixin.qrcode.type.QrCodeType;
+import nlf.plugin.weixin.util.HttpsClient;
+
+/**
+ * 二维码工具类
+ * 
+ * @author 6tail
+ *
+ */
+public class QrCodeHelper{
+  /** 创建二维码URL */
+  public static String URL_CREATE = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=?";
+  /** 显示二维码URL */
+  public static String URL_SHOW = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=?";
+
+  private QrCodeHelper(){}
+
+  /**
+   * 创建二维码
+   * 
+   * @param request 二维码请求
+   * @param accessToken
+   * @return 二维码响应
+   * @throws WeixinException
+   */
+  public static QrCodeResponse createQrCode(QrCodeRequest request,String accessToken) throws WeixinException{
+    try{
+      Bean dataBean = new Bean();
+      dataBean.set("action_name",request.getType().toString());
+      dataBean.set("action_info",new Bean().set("scene",new Bean().set("scene_id",request.getSceneId())));
+      if(QrCodeType.QR_SCENE==request.getType()){
+        dataBean.set("expire_seconds",request.getExpireIn());
+      }
+      String data = JSON.toJson(dataBean);
+      Logger.getLog().debug(Stringer.print("nlf.plugin.weixin.send",data));
+      String result = HttpsClient.post(Stringer.print(URL_CREATE,"?",accessToken),data);
+      Bean o = JSON.toBean(result);
+      int errorCode = o.getInt("errorcode",0);
+      if(0!=errorCode){
+        throw new WeixinException(errorCode,o.getString("errmsg"));
+      }
+      QrCodeResponse response = new QrCodeResponse();
+      response.setExpireIn(o.getInt("expire_seconds",1800));
+      response.setTicket(o.getString("ticket"));
+      response.setUrl(o.getString("url"));
+      return response;
+    }catch(WeixinException e){
+      throw e;
+    }catch(Exception e){
+      throw new WeixinException(e);
+    }
+  }
+
+  /**
+   * 获取二维码图片URL
+   * 
+   * @param ticket 二维码ticket
+   * @return 二维码图片的URL
+   * @throws WeixinException
+   */
+  public static String showQrCode(String ticket) throws WeixinException{
+    return Stringer.print(URL_SHOW,"?",ticket);
+  }
+}
